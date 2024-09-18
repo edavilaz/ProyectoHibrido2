ba4f41617dbdd349ed07618149be8194d0f7ba77
edavilaz <edavilaz@gmail.com>
2024-09-18 17:06:02 -0500

    ultimos cambios

--- ProyectoHibrido2/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ProyectoHibrido2.AssemblyInfo.cs
+++ ProyectoHibrido2/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ProyectoHibrido2.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoHibrido2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+16d117c5987229680e1096f466fe5378d562a53d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoHibrido2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoHibrido2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
